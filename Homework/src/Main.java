a1e8c10488a6f344b4f10db920c6f2d0bd59f220
Nodiko <nodariko.gachava@gmail.com>
2025-03-28 13:43:30 +0400

    new Branch

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        Test test = new Test(20, "Gio");
+        System.out.println(test.age);
+        System.out.println(test.name);
+
+    }
 }
---
+++ Homework/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+     int age;
+     String name;
+
+    public Test(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+}
